a93b05d7ff70e49fabefd35b3c661ea8f9b179df
Sarah-Katz <Sarahkatz59@gmail.com>
2024-04-05 15:34:01 +0200

    first commit

---
+++ src/test/java/co/simplon/burger/BurgerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.burger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
