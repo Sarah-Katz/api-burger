15fbeab969ca4cb431d7c308a442aa3d00feab63
Sarah-Katz <Sarahkatz59@gmail.com>
2024-04-06 19:00:50 +0200

    fix(Ad): fixed type in query

--- src/main/java/co/simplon/burger/persistance/repository/ad/IAdRepository.java
+++ src/main/java/co/simplon/burger/persistance/repository/ad/IAdRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import co.simplon.burger.persistance.entity.Ad;
+import co.simplon.burger.persistance.entity.User;
 
 @Repository
 public interface IAdRepository extends JpaRepository<Ad, Integer> {
     @Query(AdQueries.FIND_BY_VENDOR)
-    List<Ad> findByVendor(final String vendor);
+    List<Ad> findByVendor(final User vendor);
 }
