b4d4d19f2f13349218855189ec2924c50980fb14
Sarah-Katz <Sarahkatz59@gmail.com>
2024-08-10 19:43:46 +0200

    feat: Added Ad service + unit tests

---
+++ src/main/java/co/simplon/burger/business/service/ad/AdServiceImpl.java
@@ -0,0 +1,40 @@
+package co.simplon.burger.business.service.ad;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import co.simplon.burger.business.convert.AdConvert;
+import co.simplon.burger.business.convert.UserConvert;
+import co.simplon.burger.business.dto.AdDto;
+import co.simplon.burger.business.dto.UserDto;
+import co.simplon.burger.persistance.repository.ad.IAdRepository;
+
+@Service
+public class AdServiceImpl implements IAdService {
+    private IAdRepository adRepository;
+
+    /**
+     * Injects the required dependencies to the service.
+     * 
+     * @param adRepository
+     */
+    public AdServiceImpl(final IAdRepository adRepository) {
+        this.adRepository = adRepository;
+    }
+
+    @Override
+    public void saveAd(AdDto adDto) {
+        adRepository.save(AdConvert.getInstance().toEntity(adDto));
+    }
+
+    @Override
+    public List<AdDto> getAllAdsForVendor(UserDto user) {
+        return AdConvert.getInstance().listToDto(adRepository.findByVendor(UserConvert.getInstance().toEntity(user)));
+    }
+
+    @Override
+    public void deleteAd(int adId) {
+        adRepository.deleteById(adId);
+    }
+}
